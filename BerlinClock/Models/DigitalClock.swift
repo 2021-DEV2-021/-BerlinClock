5fa0de23dfb153706a1e913e263b224b7c35b835
2021-DEV2-021 <DEV2-021@outlook.com>
2021-04-19 13:28:47 +0200

    Added implimentation and tests DigtalCLock

--- BerlinClock/Models/DigitalClock.swift
+++ BerlinClock/Models/DigitalClock.swift
@@ -11,6 +11,9 @@ class DigitalClock: Clock {
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateClock), userInfo: nil, repeats: true)
     }
     
+    func getDigitalTime(date: Date) -> String {
+        return "\(getCurrentHour(date: date)):\(getCurrentMinute(date: date)):\(getCurrentSecond(date: date))"
+    }
     
     @objc private func updateClock() {
         date = Date.init()
