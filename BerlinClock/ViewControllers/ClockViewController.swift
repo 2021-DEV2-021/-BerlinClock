5853f64c083bd75da25c4f614e5a902e9b6422ff
2021-DEV2-021 <DEV2-021@outlook.com>
2021-04-19 13:44:43 +0200

    setup BerlinClockViewController + Dependency Injection ClockViewController

--- BerlinClock/ViewControllers/ClockViewController.swift
+++ BerlinClock/ViewControllers/ClockViewController.swift
@@ -1,12 +1,27 @@
 import UIKit
 
+fileprivate enum SegueKeys: String {
+    case digitalClockSegue
+    case berlinClockSegue
+}
+
 class ClockViewController: UIViewController {
+    
+    var berlinClock : BerlinClock!
+    var digitalClock : DigitalClock!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == SegueKeys.berlinClockSegue.rawValue) {
+            guard let berlinClockChildVC = segue.destination as? BerlinClockViewController else {
+                return
+            }
+            berlinClockChildVC.berlinClock = berlinClock
+        }
+    }
 }
 
